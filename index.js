bb086cee3100e599b69c43aafbeb681acfdb5644
Vlatko Juric <juricv78gmail.com>
2023-10-12 11:50:46 +0200

    js destrocturing objects exercise

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const company = {
   location: "Hamburg",
 };
 
+export const { course } = company;
 // example: export const {value1} = myObject;
 
 // EXERCISE 2
@@ -19,24 +20,34 @@ const company = {
 //  2.2: 'years' property into the variable 'age'.
 //  2.3: 'isAdmin' property into the variable 'isAdmin' (false, if no such property exists)
 
-const user = { name: "John", years: 30 };
-
+export const user = { name: "John", years: 30 };
+export const { name: name, years: age, isAdmin = false } = user;
+console.log(user);
 // EXERCISE 3
 // Extract the variables from the dog object and
 // rename them like "dogName", "dogBreed", and "dogAge", respectively.
 
-const dog = {
+export const dog = {
   name: "Pluto",
   breed: "husky",
   age: 5,
 };
+export const {
+  name: dogName = "Pluto",
+  breed: dogBreed = "husky",
+  age: dogAge = 5,
+} = dog;
+console.log(dog);
+console.log(dogName, dogBreed, dogAge);
 
 // EXERCISE 4
 // Extract the 'lastName' property from the person object as "personLastName".
 // Store all other properties in an object called "moreInformation".
 
-const person = {
+export const person = {
   lastName: "Main",
   age: 24,
   firstName: "Alex",
 };
+export const { lastName: personLastName = "Main" } = person;
+export const moreInformation = { age: 24, firstName: "Alex" };
